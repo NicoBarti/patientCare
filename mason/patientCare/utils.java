bd3e4a3186be8e5aef273b982f77138e2ae83447
NicoBarti <nicolas.barticevic@gmail.com>
2024-04-29 21:42:45 +0100

    create runCare, a model wraper to pass parameters

---
+++ mason/patientCare/utils.java
@@ -0,0 +1,28 @@
+package patientCare;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVWriter;
+
+public class utils {
+
+	public static void saveToCSV(int[] distr0, String finalPath) {
+		System.out.println("Saving the distribution to " + finalPath);
+		List<String[]> list = new ArrayList<>();
+		String[] frequency = {"counts"};
+		list.add(frequency);
+		for (int i = 0; i < distr0.length; i++) {
+			String[] a = {Integer.toString(distr0[i])};
+			list.add(a);
+		}
+		try (CSVWriter writer = new CSVWriter (new FileWriter(finalPath))) {
+			writer.writeAll(list);
+		} catch (IOException e) {
+			System.out.println("Problem in CS Writer "+e);
+		}
+	}
+	
+}
