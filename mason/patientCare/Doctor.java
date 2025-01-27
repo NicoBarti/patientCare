fadf7713aeffa1e9c5a99808d93ea25c4354b68d
NicoBarti <nicolas.barticevic@gmail.com>
2025-01-27 11:06:10 +0000

    flawed model 5 because noises depend on number of weeks

--- mason/patientCare/Doctor.java
+++ mason/patientCare/Doctor.java
@@ -21,9 +21,9 @@ public class Doctor implements Steppable {
 	}
 	
 
-	public double interactWithPatient(int id, double complexity) {
+	public double interactWithPatient(int id, double needs) {
 		visitCounter[id] += 1;
-		T = prescribeTreatment(visitCounter[id], complexity);
+		T = prescribeTreatment(visitCounter[id], needs);
 		A = A - 1; 
 		//conterfactual check: no complexity on treatments:
 		//return(1);
@@ -31,9 +31,9 @@ public class Doctor implements Steppable {
 	}
 	
 
-	public double prescribeTreatment(int n_visits, double complexity) {
+	public double prescribeTreatment(int n_visits, double needs) {
 		//return(1);
-		return(Math.min(n_visits/(3*complexity), t));
+		return(Math.min(Math.min(n_visits/(3*needs), t), needs));
 	}
 	
 	public void initializeDoctor(int capacity, int nPatients, double tto) {
