bf0445c4b2973fae1d39a5482c4770944169d499
NicoBarti <nicolas.barticevic@gmail.com>
2025-01-27 11:10:38 +0000

    model with NOISE = 0

--- mason/patientCare/Patient.java
+++ mason/patientCare/Patient.java
@@ -59,13 +59,13 @@ public class Patient implements Steppable {
 			progress = 1;
 			care.totalProgress = care.totalProgress + 1;
 		}
-		double noise = care.random.nextGaussian()*0.05;
+		double noise = care.random.nextGaussian()*0;
 		double finalProgress =  progress + noise;
 		H[current_week] = Math.max(0, Math.min(H[current_week-1] + finalProgress - T[current_week-1], 5));
 	}
 	
 	protected void expectationFormation(Care care) {
-		double noise = care.random.nextGaussian()*0.05;
+		double noise = care.random.nextGaussian()*0;
 		//forms the expectation for the next consultation based on previous experience
 		
 		// got the visit last week
